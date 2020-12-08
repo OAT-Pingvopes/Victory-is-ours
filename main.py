8cc3731a2721cf495d576951df0fb63aea98f5c3
Андрей <andrew121205@gmail.com>
2020-12-08 21:05:29 +0500

    Testing commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import pygame
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                position = (x * self.cell_size + self.left, y * self.cell_size + self.top)
+                size = (self.cell_size, self.cell_size)
+                pygame.draw.rect(screen, 'white', (position, size), 1)
+                if self.board[y][x] == 0:
+                    pygame.draw.rect(screen, (0, 0, 0), ((position[0] + 1, position[1] + 1),
+                                                         (size[0] - 2, size[1] - 2)), 0)
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), ((position[0] + 1, position[1] + 1),
+                                                               (size[0] - 2, size[1] - 2)), 0)
+
+    def on_click(self, cell):
+        pass
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if (cell_x < 0 or cell_x >= self.width) or (cell_y < 0 or cell_y >= self.height):
+            return None
+        for x in range(len(self.board)):
+            for y in range(len(self.board[x])):
+                if x == cell_y or y == cell_x:
+                    if self.board[x][y] == 1:
+                        self.board[x][y] = 0
+                    else:
+                        self.board[x][y] = 1
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+if __name__ == '__main__':
+    fps = 60  # количество кадров в секунду
+    clock = pygame.time.Clock()
+    pygame.init()
+    pygame.display.set_caption('Чёрное в белое и наоборот')
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+    board = Board(4, 3)
+    board.set_view(100, 100, 50)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
+        board.render()
+        pygame.display.flip()
+        clock.tick(50)
+    pygame.quit()
