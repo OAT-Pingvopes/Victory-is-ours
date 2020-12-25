fe966dce172d46e617c6ce8a755b7a74d65c5cab
Artyom <Artemmailsbox@gmail.com>
2020-12-25 19:38:28 +0500

    join to codes

--- first.py
+++ first.py
@@ -1,15 +1,76 @@
-import pygame
 import random
 from pygame.locals import *
-
+import pygame
 pygame.init()
 # 0 - вода
 # 1 - выделенная клетка
 # 2 - земля
+# 3 - железо
+# 4 - дерево
+# 5 - нефть
+# 6 -
+# 7 -
+# 8 -
+# 9 - артиллерия
 # 10 - пехота
 # 20 - мотопехота
 # 30 - танк
+# 40 - самолёт
+# 50 - вертолёт
 all_sprites = pygame.sprite.Group()
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+        nick = open('data/cfg.txt', mode='r').readlines()[0].split()[2]
+        self.txt_surface = FONT.render(nick[1:-1], True, self.color)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                    # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def register(self):
+        nick = open('data/cfg.txt', mode='r').readlines()[0].split()
+        nick[2] = "'" + self.text + "'"
+        f = open('data/cfg.txt', mode='w')
+        f.write(' '.join(nick))
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
 
 
 class Button:
@@ -102,14 +163,8 @@ class Board:
                 position = (x * self.cell_size + self.left, y * self.cell_size + self.top)
                 size = self.cell_size, self.cell_size
                 pygame.draw.rect(screen, (128, 128, 128), (position, size), 1)
-                if self.board[y][x] == 1:
-                    pygame.draw.rect(screen, (255, 255, 255), ((position[0] + 1, position[1] + 1),
-                                                               (size[0] - 2, size[1] - 2)), width=4)
-                    pygame.draw.rect(screen, (0, 255, 0), ((position[0] + 3, position[1] + 3),
-                                                           (size[0] - 6, size[1] - 6)), 0)
-                else:
-                    pygame.draw.rect(screen, self.color[int(self.board[y][x])], ((position[0] + 1, position[1] + 1),
-                                                                                (size[0] - 2, size[1] - 2)), 0)
+                pygame.draw.rect(screen, self.color[int(self.board[y][x])], ((position[0] + 1, position[1] + 1),
+                                                                             (size[0] - 2, size[1] - 2)), 0)
 
     def on_click(self, cell):
         pass
@@ -140,17 +195,21 @@ class Board:
 
     def menu(self):
         background = pygame.image.load('data/start_menu.png')
+        reg = Button()
         close = Button()
         start = Button()
         save = Button()
         load = Button()
+        input_box1 = InputBox(1700, 1000, 200, 32)
+        input_boxes = [input_box1]
         show = True
         while show:
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.nick = open('data/cfg.txt', mode='r').readlines()[0].split()[2][1:-1]
                     if close.pressed(event.pos):
                         exit()
-                    elif start.pressed(event.pos):
+                    elif start.pressed(event.pos) and self.nick:
                         start.create_button(screen, (34, 139, 34), 860, 430, 200, 50, 100,
                                             'Продолжить', (255, 255, 255))
                         show = False
@@ -163,17 +222,30 @@ class Board:
                         show = False
                     elif load.pressed(event.pos):
                         pass
+                    if reg.pressed(event.pos):
+                        input_box1.register()
+
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE and self.b == 1:
                         show = False
+                for box in input_boxes:
+                    box.handle_event(event)
+
+            for box in input_boxes:
+                box.update()
             screen.blit(background, (0, 0))
-            close.create_button(screen, (34, 139, 34), 860, 700, 200, 50, 100, 'Выйти', (255, 255, 255))
+            for box in input_boxes:
+                box.draw(screen)
+            close.create_button(screen, (34, 139, 34), 860, 520, 200, 50, 100, 'Выйти', (255, 255, 255))
+            reg.create_button(screen, (34, 139, 34), 1450, 991, 200, 50, 100,
+                              'Принять Ник', (255, 255, 255))
             save.create_button(screen, (34, 139, 34), 860, 520, 200, 50, 100, 'Сохранить', (255, 255, 255))
             load.create_button(screen, (34, 139, 34), 860, 610, 200, 50, 100, 'Загрузить', (255, 255, 255))
             if self.b == 0:
                 start.create_button(screen, (34, 139, 34), 860, 430, 200, 50, 100, 'Старт', (255, 255, 255))
             else:
                 start.create_button(screen, (34, 139, 34), 860, 430, 200, 50, 100, 'Продолжить', (255, 255, 255))
+
             pygame.display.update()
             clock.tick(60)
         screen.fill('black')
@@ -189,9 +261,9 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Victory is ours')
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    board = Board(38, 21)
+    board = Board(62, 35)
     board.menu()
-    board.set_view(20, 30, 50)
+    board.set_view(60, 30, 30)
     running = True
     while running:
         for event in pygame.event.get():
